bcab6047b367ff2b1890f305bb92f9cfac67f243
SIddharth Mathur <116292769+SiddharthMathur01@users.noreply.github.com>
2026-05-05 20:55:30 +0530

    deployment fix

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import lightgbm as gbm
 import re
 import math
 
-model = gbm.Booster(model_file=r"CyberSecurity\Password-Strength-Checker\modle\password_strength_model.txt")
+modle = gbm.Booster(modle_file="modle/password_strength_model.txt")
 
 words= {"password", "admin", "welcome", "login", "user","qwerty", "abc", "letmein", "iloveyou", "monkey","dragon", "football", "india", "love", "boss","google", "facebook", "123", "1234", "12345","123456"}
 leet= str.maketrans({"0": "o","1": "i","3": "e","4": "a","5": "s","7": "t","@": "a","$": "s"})
